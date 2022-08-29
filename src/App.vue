796f8a9ac8743da25cac378aafdca6d1918a06f4
Matheus Rocha <matheus.srocha@yahoo.com.br>
2022-08-28 23:32:10 -0300

    Definição do template das páginas do projeto

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <OrganismHeader />
+    <RouterView />
+</template>
+
+<script>
+import OrganismHeader from './components/organism/OrganismHeader.vue';
+
+export default {
+    components: { OrganismHeader }
+}
+</script>
